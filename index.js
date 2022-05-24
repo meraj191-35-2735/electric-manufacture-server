0c08158ac5b9d95be82795981f53b89e41fe56dd
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-05-24 21:20:32 +0600

    dynamic url added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const ObjectId = require("mongodb").ObjectId;
+// const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5t0iy.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,10 +33,9 @@ async function run() {
       const tools = await cursor.toArray();
       res.send(tools);
     });
-
     app.get("/tool/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: Object(id) };
+      const query = { _id: ObjectId(id) };
       const result = await toolCollection.findOne(query);
       res.send(result);
     });
